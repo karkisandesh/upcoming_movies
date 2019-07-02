5cf450d9a761f9867cbff97fc0f964437e0125d2
Sandesh Karki <karkisandesh@hotmail.com>
2019-07-02 22:53:37 +0000

    create cli file

---
+++ NOTES.md
@@ -0,0 +1,10 @@
+- CLI
+- upcoming_movies
+- scraper
+  - movie name
+  - release date
+  - time
+
+- Choose for details
+ - Synopsis
+ 
--- lib/upcoming_movies.rb
+++ lib/upcoming_movies.rb
@@ -1,4 +1,5 @@
-require "upcoming_movies/version"
+require_relative "upcoming_movies/version"
+require_relative "upcoming_movies/cli"
 
 module UpcomingMovies
   class Error < StandardError; end
