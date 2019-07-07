6f55087b3a275448a1c079313673a840935d0018
Sandesh Karki <karkisandesh@hotmail.com>
2019-07-07 23:21:30 +0000

    pseudo scraping

--- lib/upcoming_movies/cli.rb
+++ lib/upcoming_movies/cli.rb
@@ -4,26 +4,48 @@ class UpcomingMovies::CLI
     puts "----------------------------******-----------------------------"
     puts "Welcome to the Upcoming Movies event!"
     puts ""
-    puts "Please follow the instruction to view the movies you would like."
-    puts ""
-    puts "Type list to list the movies or exit"
+    puts "Would you like to view the upcoming movies? y/n"
+    input = gets.strip.downcase
 
-    input = gets.strip
-      if input == "list".upcase
-        list
-      else
-        goodbye
+    if input == 'y'
+        list_movies
+        select_movies
+    else
+      goodbye
+    end
+  end
+
+  def get_movies
+    @movies = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+  end
+
+  def list_movies
+      get_movies.each.with_index(1) do |index, movie|
+        puts "#{index}. #{movie}"
       end
   end
 
-  def list
-      puts ""
-      puts "hello"
+  def select_movies
+    puts ""
+    puts "Please select a movie for details (1-8)."
+    input = gets.strip
+    if input.to_i > 0 && input.to_i <= 8
+        list_movie_details
+      else
+        puts "Please select a movie for details (1-8) or exit."
+        input = gets.strip
+        goodbye if input == "exit"
+      end
+  end
+
+  def list_movie_details
+    puts ""
+    puts "Full movie details"
   end
 
   def goodbye
-    puts "Thank you."
+    puts ""
+    puts "Thank You."
   end
-
 
 end
