e82a4d5203c763a6cb4c01566fef1e7199aabc0a
Sandesh Karki <karkisandesh@hotmail.com>
2019-07-07 19:05:23 +0000

    listing movies

--- lib/upcoming_movies/cli.rb
+++ lib/upcoming_movies/cli.rb
@@ -1,10 +1,29 @@
 class UpcomingMovies::CLI
 
   def call
-    puts "Hello"
-    binding.pry
+    puts "----------------------------******-----------------------------"
+    puts "Welcome to the Upcoming Movies event!"
+    puts ""
+    puts "Please follow the instruction to view the movies you would like."
+    puts ""
+    puts "Type list to list the movies or exit"
+
+    input = gets.strip
+      if input == "list".upcase
+        list
+      else
+        goodbye
+      end
   end
 
+  def list
+      puts ""
+      puts "hello"
+  end
+
+  def goodbye
+    puts "Thank you."
+  end
 
 
 end
