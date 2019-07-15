76779ae19d9b31d75820fa0512027f5007d58ac7
Sandesh Karki <karkisandesh@hotmail.com>
2019-07-15 23:06:11 +0000

    scraping from urls

--- lib/upcoming_movies/scraper.rb
+++ lib/upcoming_movies/scraper.rb
@@ -1,5 +1,14 @@
 class UpcomingMovies::Scraper
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+   @@all << self
+ end
 
   def scrape_movies
     doc = Nokogiri::HTML(open("https://www.fandango.com/"))
@@ -13,11 +22,25 @@ class UpcomingMovies::Scraper
 
   end
 
-  def self.scrape_details
+  def scrape_hrefs
     doc = Nokogiri::HTML(open("https://www.fandango.com/"))
     urls = doc.css(".footer-coming-soon--info-block.poster-thumb-size-s a.heading-style-1.footer-coming-soon--heading").collect do |anchor|
        anchor["href"]
     end
+  end
+
+  def self.scrape_details(index = "1")
+   urls = UpcomingMovies::Scraper.new.scrape_hrefs
+   doc = Nokogiri::HTML(open("#{urls[index.to_i - 1]}"))
+
+   doc.css("ul.movie-details__detail").collect do |details|
+     binding.pry
+    #  new_movie = UpcomingMovies::New_movies.new
+     title = details.search("h1.subnav__title").text.strip
+
+     title = details.search("li.movie-details__release-date").text.strip
+     binding.pry
+   end
   end
 
 
